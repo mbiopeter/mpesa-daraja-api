dec9757132cecdb79d5ea6de35a6c43a2bf3befe
mbiopeter <mbiopeter401@gmail.com>
2024-03-18 15:39:55 +0300

    seconf commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'Yk4A7dIqLYpRxS9PPftxPo3BtN6EvGdpWYr5pwk2LcxBGU64'; //Fill with your app Consumer Key
+  $consumerSecret = 'RepseOTlgg5rtlHRWq6cIdlSz2GAy88XkaT3PKzoNvPlKvqcqqq6tAMdJTrGcXbF'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://morning-basin-87523.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://frozen-falls-10456-cfc818db19e7.herokuapp.com/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
